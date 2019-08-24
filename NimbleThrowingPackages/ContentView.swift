92b6bd7f6c7553cfbe6e79a3cba304902ceaf253
Tom Drummond <tom@devioustree.co.uk>
2019-08-24 11:23:36 -0700

    Initial Commit

---
+++ NimbleThrowingPackages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NimbleThrowingPackages
+//
+//  Created by Tom Drummond on 24/08/2019.
+//  Copyright © 2019 Tom Drummond. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
